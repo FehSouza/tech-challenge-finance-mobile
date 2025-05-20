d98e3dd18ace69c942af1c19d1df0e58b83296ff
raulpesilva <adraulpesilva@gmail.com>
2025-05-20 16:06:38 -0300

    feat: implement search functionality by adding state management for search term in Search component

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { SearchIcon } from '../icons';
 import { Input } from '../shared';
 
 export const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <View style={style.container}>
-      <Input iconLeft={<SearchIcon />} placeholder='Pesquisa' inputMode='search'/>
+      <Input
+        iconLeft={<SearchIcon />}
+        placeholder='Pesquisa'
+        inputMode='search'
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+      />
     </View>
   );
 };

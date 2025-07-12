361ab3d3f8231fdeabb152cfc1a2baa2bacf0125
raulpesilva <adraulpesilva@gmail.com>
2025-07-12 18:04:30 -0300

    refactor: simplify transaction deletion logic and improve code structure

--- app/edit-transaction/[id].tsx
+++ app/edit-transaction/[id].tsx
@@ -4,30 +4,31 @@ import { theme } from '@/theme';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function EditTransaction() {
+const useEditTransaction = () => {
   const router = useRouter();
   const { id } = useLocalSearchParams();
-  if (!id) return null;
 
   const handleDeleteTransaction = () => {
-    if (Array.isArray(id)) {
-      console.error('ID should not be an array');
-      return;
-    }
+    if (Array.isArray(id)) return console.error('ID should not be an array');
     deleteTransaction(id);
     router.back();
   };
+
+  return { id, handleDeleteTransaction };
+};
+
+export default function EditTransaction() {
+  const { id, handleDeleteTransaction } = useEditTransaction();
+  if (!id) return null;
 
   return (
     <ContainerKeyboardAvoiding>
       <View style={style.container}>
         <Text style={style.title}>Editar transação</Text>
-
         <ButtonIcon color='error' onPress={handleDeleteTransaction}>
           <TrashIcon />
         </ButtonIcon>
       </View>
-
       <FormTransaction id={Array.isArray(id) ? id[0] : id} />
     </ContainerKeyboardAvoiding>
   );
